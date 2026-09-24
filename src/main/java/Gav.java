449904ae70ef1bf87f8b7fb44440fddae7012c6b
lydiaamrouche <lydiaamrouche22@gmail.com>
2026-09-24 12:52:00 +0200

    TD2 Q6 green - analyse des coordonnees Gav

--- src/main/java/Gav.java
+++ src/main/java/Gav.java
@@ -1,9 +1,28 @@
 public class Gav {
+    private final String group;
+    private final String artifact;
+    private final String version;
+
+    private Gav(String group, String artifact, String version) {
+        this.group = group;
+        this.artifact = artifact;
+        this.version = version;
+    }
+
     public static Gav parse(String texte) {
-        return new Gav();
+        String[] parties = texte.split(":");
+        return new Gav(parties[0], parties[1], parties[2]);
     }
 
     public String group() {
-        return "org.acme";
+        return group;
+    }
+
+    public String artifact() {
+        return artifact;
+    }
+
+    public String version() {
+        return version;
     }
 }
